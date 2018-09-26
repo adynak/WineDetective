1e1d30fbb8a8f21765c9d223ebb800d6752baeeb
AL DYNAK <adynak@gmail.com>
2018-09-26 10:18:44 -0700

    postgresql functions

--- sql/varietal.sql
+++ sql/varietal.sql
@@ -89,3 +89,90 @@ BEGIN
 END; $$ 
 
 LANGUAGE plpgsql;
+
+**************************************************************************************************************
+-- select * from winedetective.get_smart('Tempernillo'); for example
+drop function winedetective.get_smart;
+CREATE OR REPLACE FUNCTION winedetective.get_smart (p_varietal text) 
+ RETURNS TABLE (
+  vintage INT,
+  varietal text,
+  producer text,
+  vineyard text,
+  bins text
+) AS $$
+DECLARE 
+    var_r record;
+BEGIN
+ FOR var_r IN (
+  SELECT distinct
+    bottle.vintage,
+    bottle.varietal, 
+    bottle.producer,
+    bottle.vineyard,
+    winedetective.get_bins(concat(category,bottle.varietal,bottle.vintage,bottle.producer,bottle.vineyard)) as bins
+    FROM 
+    winedetective.bottle
+  WHERE
+    bottle.varietal = p_varietal
+  )  
+ LOOP
+    vintage := var_r.vintage;
+    varietal := var_r.varietal ; 
+    producer := var_r.producer ; 
+    vineyard := var_r.vineyard ; 
+    bins:= var_r.bins;
+        RETURN NEXT;
+ END LOOP;
+END; $$ 
+LANGUAGE 'plpgsql';
+
+**************************************************************************************************************
+
+drop function winedetective.get_bins ;
+
+CREATE OR REPLACE FUNCTION winedetective.get_bins(searchpattern text)
+  RETURNS text AS $$
+
+-- searchpattern is 'red, 'white', 'other'
+-- returns something like this : {"red":[{"name":"Cabernet"},{"name":"Pinot Noir"},{"name":"Tempernillo"}]}
+
+DECLARE 
+  counter       INTEGER := 0 ; 
+  json          TEXT DEFAULT '';
+  stanza        TEXT DEFAULT '';
+  recordbottle  RECORD;
+  cursorbottles CURSOR for select bin from winedetective.bottle where concat(category,varietal,vintage,producer,vineyard) = searchpattern ;
+
+  BEGIN
+    -- Open the cursor
+    OPEN cursorbottles;
+
+    json = '{"' || 'bins' || '":[';
+
+    LOOP
+      -- fetch row into the film
+      FETCH cursorbottles INTO recordbottle;
+      -- exit when no more row to fetch
+      EXIT WHEN NOT FOUND;
+
+      -- build the output
+      if (counter = 0) then
+        stanza := stanza ||  '{' || '"name":"' ||  recordbottle.bin || '"}';
+      else
+        stanza := stanza || ',{' || '"name":"' ||  recordbottle.bin || '"}';
+      end if;
+
+      counter := counter + 1 ; 
+
+    END LOOP;
+
+    json := json || stanza || ']}';
+    
+    -- Close the cursor
+    CLOSE cursorbottles;
+
+RETURN json;
+END; $$
+
+LANGUAGE plpgsql;
